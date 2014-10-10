a1370b82e9d3b9190d9afda7b1f2cad8810306c8
Matthias Hölzl <tc@xantira.com>
2014-10-10 03:59:05 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -5,10 +5,10 @@
             :url "http://unlicense.org/UNLICENSE"
             :distribution :repo}
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2311" :scope "provided"]
-                 [org.clojure/google-closure-library "0.0-20140226-71326067" :scope "provided"]
-                 [http-kit "2.1.18"]]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha2"]
+                 [org.clojure/clojurescript "0.0-2356" :scope "provided"]
+                 [org.clojure/google-closure-library "0.0-20140718-946a7d39" :scope "provided"]
+                 [http-kit "2.1.19"]]
 
   :profiles {:dev {:dependencies [[com.cemerick/piggieback "0.1.3"]]}}
   :source-paths ["src/clj" "src/cljs"]
